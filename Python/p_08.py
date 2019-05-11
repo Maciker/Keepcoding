db03f56b25f0910cfc946dae39ec4d24ff7aa7de
Iker Macaya Faber <42570380+Maciker@users.noreply.github.com>
2019-05-11 10:42:07 +0200

    Add files via upload

---
+++ Python/p_08.py
@@ -0,0 +1,12 @@
+# suma los 100 primeros numeros
+suma = 0
+for n in range(101):
+    suma +=n
+
+# Con el while
+# i = 1
+# while i < 101:
+    # suma += i
+    # i += 1
+
+print("La suma de los 100 primeros numeros es: ", suma)
