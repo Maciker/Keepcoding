d77895543e5dd71bec6877535a3953d0f6e1560c
Iker Macaya Faber <42570380+Maciker@users.noreply.github.com>
2019-05-11 09:42:47 +0200

    Add files via upload

---
+++ Python/p_05.py
@@ -0,0 +1,24 @@
+# La torutuga
+# Importamos librera turtle para dibujar un cuadrado.
+# Lo hacemos mediante una funcion para no repetir codigo.
+import turtle
+
+def cuadrado():
+    miT.forward(50)
+    miT.left(90)
+    miT.forward(50)
+    miT.left(90)
+    miT.forward(50)
+    miT.left(90)
+    miT.forward(50)
+    miT.left(90)
+
+miT = turtle.Turtle()
+cuadrado()
+miT.left(90)
+cuadrado()
+miT.left(90)
+cuadrado()
+miT.left(90)
+cuadrado()
+miT.left(90)
