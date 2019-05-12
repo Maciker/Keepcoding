0a442d24a478720e9d79feb1f5b9db96956fd25d
Iker Macaya Faber <42570380+Maciker@users.noreply.github.com>
2019-05-12 21:09:06 +0200

    Add files via upload

---
+++ Python/p_13_cadenas.py
@@ -0,0 +1,25 @@
+# Cadenas y codificacion
+cadena = 'h o l a'
+cadenaMayus = ''
+
+for i in range(len(cadena)):
+    print (cadena[i], " - ", ord(cadena[i]))
+    # Vamos a ponerlo en mayusculas restando 32 al valor del caracter
+    codigo=ord(cadena[i])-32
+    cadenaMayus += chr(codigo)
+
+print(cadenaMayus)
+
+# Funcion que cuenta el numero de palabras de una frase.
+def cuentaPalabras(cadena):
+    frase = cadena.split()
+    return len(frase)
+
+cadena = 'Aqui hay cuatro palabras '
+print (cadena, cuentaPalabras(cadena))
+
+def cuentaCaracteres(cadena):
+    for i in range(len(cadena)):
+
+
+
